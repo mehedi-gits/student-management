d8660726b48f8a15bd7542b3d564588c150adf2d
Mehedi <myandypo@gmail.com>
2025-03-14 16:48:08 +0000

    Changes in files

--- student/views.py
+++ student/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def homepage(request):
+
+    student = [
+        {
+        'Name': 'Name1',
+        'Email': 'Email1',
+        'Phone': 'Phone1',
+        'Course': 'Course1'
+        },
+        {
+        'Name': 'Name2',
+        'Email': 'Email2',
+        'Phone': 'Phone2',
+        'Course': 'Course2'
+        },
+        {
+        'Name': 'Name3',
+        'Email': 'Email3',
+        'Phone': 'Phone3',
+        'Course': 'Course3'
+        }
+    ]
+
+    context = {
+        'student': student
+        }
+
+    return render(request, 'student/home.html', context)
--- student_management/settings.py
+++ student_management/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&+2a1$1tn5(0-mblcm#+5an46s)f(+aq8&tgf4yw7c(mw915aw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.1.100']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'student',
 ]
 
 MIDDLEWARE = [
